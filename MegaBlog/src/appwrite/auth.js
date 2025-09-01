cfe0365fae90c22857c50eefd4d1d98200fc1c51
Priyank1905 <priyankjethva1905@gmail.com>
2025-09-02 00:48:44 +0530

    feat: implement AuthService for user account management

---
+++ MegaBlog/src/appwrite/auth.js
@@ -0,0 +1,32 @@
+import conf from '../conf/conf';
+import { Client, Account, ID } from 'appwrite';
+
+export class AuthService {
+    client = new Client();
+    account;
+
+    constructor() {
+        this.client
+            .setEndpoint(conf.appwriteUrl)
+            .setProject(conf.appwriteProjectId);
+        this.account = new Account(this.client)
+    }
+
+    async createAccount({ email, password, name }) {
+        try {
+            const userAccount = await this.account.create(ID.unique(), email, password, name)
+            if(userAccount) {
+                
+            }else {
+                return userAccount;
+            }
+        } catch (error) {
+            throw error;
+        }
+    }
+}
+
+const authService = new AuthService();
+
+export default authService
+
