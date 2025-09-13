01e1e6d7b909d11c89f25620b188e225dfd79a7a
Priyank1905 <priyankjethva1905@gmail.com>
2025-09-13 10:40:59 +0530

    add Signup component for user registration and form handling

--- MegaBlog/src/components/Signup.jsx
+++ MegaBlog/src/components/Signup.jsx
@@ -1,57 +1,58 @@
 import React, { useState } from 'react'
 import { Link, useNavigate } from 'react-router-dom'
-import { login as authLogin } from '../store/authSlice'
+import { login } from '../store/authSlice'
 import { useDispatch } from 'react-redux'
 import authService from '../appwrite/auth'
 import { Button, Input, Logo } from './index'
 import { useForm } from 'react-hook-form'
 
-
-
-function Login() {
+function Signup() {
     const navigate = useNavigate()
     const dispatch = useDispatch()
     const { register, handleSubmit } = useForm()
     const [error, setError] = useState('')
 
-    const login = async (data) => {
+    const creat = async (data) => {
         console.log(data);
         setError('');
         try {
-            const session = await authService.login(data)
-            if (session) {
-                const userData = authService.getCurrentUser()
-                dispatch(authLogin(userData))
+            const userData = await authService.createAccount(data);
+            if (userData) {
+                const userData = await authService.getCurrentUser()
+                if (userData) dispatch(login(userData));
                 navigate('/')
             }
         } catch (error) {
             setError(error.message)
         }
     }
-
     return (
-        <div
-            className='flex items-center justify-center w-full'
-        >
+        <div className="flex items-center justify-center">
             <div className={`mx-auto w-full max-w-lg bg-gray-100 rounded-xl p-10 border border-black/10`}>
                 <div className="mb-2 flex justify-center">
                     <span className="inline-block w-full max-w-[100px]">
                         <Logo width="100%" />
                     </span>
                 </div>
-                <h2 className="text-center text-2xl font-bold leading-tight">Sign in to your account</h2>
+                <h2 className="text-center text-2xl font-bold leading-tight">Sign up to create account</h2>
                 <p className="mt-2 text-center text-base text-black/60">
-                    Don&apos;t have any account?&nbsp;
+                    Already have an account?&nbsp;
                     <Link
-                        to="/signup"
+                        to="/login"
                         className="font-medium text-primary transition-all duration-200 hover:underline"
                     >
-                        Sign Up
+                        Sign In
                     </Link>
                 </p>
-                {error && <p className='text-red-500 text-center mt-8'>{error}</p>}
-                <form onSubmit={handleSubmit(login)} className="mt-8">
-                    <div className='space-5'>
+                {error && <p className="text-red-600 mt-8 text-center">{error}</p>}
+
+                <form onSubmit={handleSubmit(creat)} >
+                    <div className='space-y-5'>
+                        <Input
+                            label="Full Name"
+                            placeholder="Enter full name"
+                            {...register("name", { required: true })}
+                        />
                         <Input
                             label="Email"
                             type="email"
@@ -68,18 +69,9 @@ function Login() {
                             label="Password"
                             type="password"
                             placeholder="Enter password"
-                            {...register("password", {
-                                required: true,
-                                minLength: {
-                                    value: 6,
-                                    message: "Password must have at least 6 characters"
-                                }
-                            })}
+                            {...register("password", { required: true, minLength: 6 })}
                         />
-                        <Button
-                            type="submit"
-                            className="w-full"
-                        >Sign in</Button>
+                        <Button type="submit" className="w-full">Create Account</Button>
                     </div>
                 </form>
             </div>
@@ -87,4 +79,4 @@ function Login() {
     )
 }
 
-export default Login
+export default Signup
